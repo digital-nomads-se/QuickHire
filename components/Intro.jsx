202b4c649d5a132a8da7c18ff1dfd6f065bc1413
chaitanyagawande <cgawande12@gmail.com>
2023-12-05 22:31:24 -0800

    added homepage

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -20,64 +20,39 @@ export default function Intro() {
 
   return (
     <>
-      <div class="dream-job-section text-center bg-light py-5">
+      <br /><br /><br />
+      <section class="dream-job-section text-center py-5">
         <div class="container">
-          <h1 class="display-4">Your Dream Job. Now.</h1>
-          <p class="lead">Showcase yourself to a curated list of top companies</p>
+          <h1 class="display-4" style={{ color: 'black' }}>Your Dream Job. Now.</h1>
+          <p class="lead" style={{ color: 'black' }}>Showcase yourself to a curated list of top companies</p>
+        </div>
+      </section>
+      <hr />
+
+      <div>
+        <div className="container-fluid">
+          <div className="row">
+            <img src="company-logo.png" className="img-fluid" alt="Company logo" />
+          </div>
         </div>
       </div>
+      <hr />
 
-
-      <div class="my-5">
-        <div class="container">
-          <div class="row">
-            <img src="company-logo.png" class="img-fluid" alt="Company logo" />
+      <div>
+        <div className="container-fluid">
+          <div className="row">
+            <img src="image.png" className="img-fluid" alt="Company logo" />
           </div>
         </div>
       </div>
 
-        <div class="benefits-section py-5">
-          <div class="container">
-            <div class="row justify-content-center mb-4">
-              <div class="col-md-8">
-                <h3>5X higher response from companies</h3>
-                <p>Tired of having your applications ignored by companies? Instahyre only matches you with the right jobs, leading to 5X response rates from companies compared to other job sites!</p>
-              </div>
-              <div class="col-md-4 d-flex justify-content-center">
-                <img src="response.png" alt="Higher Response" class="img-fluid" />
-              </div>
-            </div>
-
-            <div class="row justify-content-center mb-4">
-              <div class="col-md-8">
-                <h3>Your perfect job, delivered to you</h3>
-                <p>Do you really want to look at thousands of irrelevant job postings or get spammed by job sites? At Instahyre, we do the work of finding the best jobs for you, so you can sit back and relax.</p>
-              </div>
-              <div class="col-md-4 d-flex justify-content-center">
-                <img src="target.png" alt="Perfect Job" class="img-fluid" />
-              </div>
-            </div>
-
-            <div class="row justify-content-center">
-              <div class="col-md-8">
-                <h3>Privacy guaranteed</h3>
-                <p>We value your privacy very highly. You can block specific companies from viewing your profile. Or you can block all companies and just apply to specific ones yourself.</p>
-              </div>
-              <div class="col-md-4 d-flex justify-content-center">
-                <img src="privacy.png" alt="Privacy Guaranteed" class="img-fluid" />
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-      <footer class="bg-light py-4">
-        <div class="container text-center">
-            <p>&copy; 2023 Instahyre - Your Dream Job Awaits</p>
+      <footer className="bg-light py-4">
+        <div className="container text-center">
+          <p>&copy; 2023 Instahyre - Your Dream Job Awaits</p>
         </div>
       </footer>
-              
-          </>
+
+    </>
   )
 }
 
