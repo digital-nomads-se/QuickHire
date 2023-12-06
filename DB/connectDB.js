ebb9e09fc4fbfdb0719955716580c4bb965d67e7
chaitanyagawande <cgawande12@gmail.com>
2023-12-06 00:02:37 -0800

    increased timeout.

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -1,24 +1,18 @@
 import mongoose from 'mongoose';
 import logger from '../Utils/logger.js';
 
-// connecting to database
 const connectDB = async () => {
 
-    // Get the connection URL from the environment variables
     const connectionUrl = process.env.DB_URI;
 
-    // Connect to the database
-    mongoose.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true, bufferCommands: false, bufferTimeoutMS: 0 })
+    mongoose.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 120000 })
         .then(() => {
-            console.log(`Database connected successfully`);
             logger.info(`Database connected successfully`);
         })
         .catch((err) => {
-            console.log("Getting Error from DB connection" + err.message);
             logger.error(`Getting Error from DB connection ${err.message}`);
         });
 
-    // Set mongoose to not enforce its default strict mode
     mongoose.set('strictQuery', false);
 };
 
